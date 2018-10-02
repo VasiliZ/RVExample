5161aed1bac2a1410f120e40b9e8ce471325f5f9
VasiliZ <Zaicev.Vasili@gmail.com>
2018-10-02 10:08:13 +0300

    Clear project

--- app/src/main/java/com/github/vasiliz/myapplication/MainActivity.java
+++ app/src/main/java/com/github/vasiliz/myapplication/MainActivity.java
@@ -1,32 +1,16 @@
 package com.github.vasiliz.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Model> modelList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setData();
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-
-        DataAdapter dataAdapter = new DataAdapter(this, modelList);
-        recyclerView.setAdapter(dataAdapter);
-    }
-
-    private void setData(){
-        for (int i =0; i<10;i++){
-            modelList.add(new Model(i));
-        }
     }
 }
